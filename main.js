13ba15c702b5765f167aa13f47675e0cd34fa5f8
Alex Martell <Alexmartell6@gmail.com>
2019-05-23 15:50:58 -0400

    created classes and search bar

--- main.js
+++ main.js
@@ -1,19 +1,21 @@
+//const mon = prompt("Enter a pokemon name or ID#").toLowerCase()
 // https://fizal.me/pokeapi/api/v2/name/<name>.json
-const mon = prompt("Enter a pokemon name or ID#").toLowerCase()
-requestAPI = () => {
+requestAPI = (mon) => {
+  var mon = prompt("Enter a pokemon name or ID#").toLowerCase()
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
             let data = JSON.parse(this.responseText)
-            user = {
-                name: data['name'],
-                hp: data['stats'][5]['base_stat'],
-                attack: data['stats'][4]['base_stat'],
-                defense: data['stats'][3]['base_stat'],
-                speed: data['stats'][0]['base_stat'],
-                ability1: data['abilities'][0]['ability']['name'],
-                ability2: data['abilities'][1]['ability']['name']
-            }
+            user = new Pokemon (
+                 data['name'],
+                 data['stats'][5]['base_stat'],
+                 data['stats'][4]['base_stat'],
+                 data['stats'][3]['base_stat'],
+                 data['stats'][0]['base_stat'],
+                 data['abilities'][0]['ability']['name'],
+                 data['abilities'][1]['ability']['name']
+            )
+            setTimeout(displayUser(mon), 300)
         }
     };
     if (isNaN(mon)){
@@ -23,10 +25,10 @@ requestAPI = () => {
     }
     xhttp.send();
 }
-requestAPI()
 
-displayUser = () => {
-    let cont = document.createElement('div')
+let cont = document.createElement('div')
+document.body.appendChild(cont)
+displayUser = (url) => {
     let h1 = document.getElementById('nme')
     let p1 = document.getElementById('hp')
     let p2 = document.getElementById('atk')
@@ -36,10 +38,10 @@ displayUser = () => {
     let cap = document.getElementById('caption')
 
     h1.innerText = user['name']
-    if (isNaN(mon)) {
-      img.setAttribute('src', `https://play.pokemonshowdown.com/sprites/bw/${mon}.png`)
+    if (isNaN(url)) {
+      img.setAttribute('src', `https://play.pokemonshowdown.com/sprites/bw/${url}.png`)
     } else {
-      img.setAttribute('src', `http://www.pokestadium.com/assets/img/sprites/${mon}.png`)
+      img.setAttribute('src', `http://www.pokestadium.com/assets/img/sprites/${url}.png`)
     }
 
     p1.innerText = `Hp: ${user['hp']}`
@@ -52,8 +54,34 @@ displayUser = () => {
     cont.appendChild(p2)
     cont.appendChild(p3)
     cont.appendChild(p4)
-    document.body.appendChild(cont)
+    a = document.body.getElementsByClassName('screen')
     cont.classList.add('screen')
 }
 
-setTimeout(displayUser, 300)
+
+class Trainer {
+  constructor(name) {
+    this.trainerName = name
+    this.buddy = []
+  }
+  get(mon){
+    requestAPI(mon)
+    this.buddy.push(mon['name'])
+
+  }
+  all(){
+
+  }
+}
+
+class Pokemon {
+  constructor(name, hp, attack, defense, speed, ability1, ability2){
+    this.name = name
+    this.hp = hp
+    this.attack = attack
+    this.defense = defense
+    this.speed = speed
+    this. ability1 = ability1
+    this.ability2 = ability2
+  }
+}
